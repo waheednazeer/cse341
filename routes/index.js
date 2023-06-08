084d39029b72fb2897a49d8df4e88231e77aa968
waheednazeer <waheed.nazeer@hotmail.com>
2023-06-08 16:32:28 +0500

    .

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Sarah Birch');
 });
+routes.get('/waheed', (req, res) => {
+    res.send('Waheed Nazir');
+  });
 
 module.exports = routes;
